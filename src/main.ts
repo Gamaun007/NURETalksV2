860b11021e355e6dd38e79a4b7b5729c3dddeb0e
roh <sergey.roh@nixsolutions.com>
2021-09-11 20:26:00 +0300

    Install all required dependencies (NGRX,Tailwind,Firebase) etc.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { TextEncoder } from 'text-encoding';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// polyfill TextEncoder for IE Edge
+if (typeof (window as any).TextEncoder === 'undefined') {
+  (window as any).TextEncoder = TextEncoder;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+});
