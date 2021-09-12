e817ab46226d49810168fdfce0a15f278c0f10ab
roh <sergey.roh@nixsolutions.com>
2021-09-12 19:16:10 +0300

    Add configuration file and config service. Add some scripts

---
+++ src/app/core/models/configuration-file.model.ts
@@ -0,0 +1,20 @@
+export interface InternationalizationConfiguration {
+  defaultLanguage: string;
+}
+
+export interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+  fireStoreEndpoints: string[];
+}
+
+export interface ConfigurationFile {
+  firebase?: FirebaseConfig;
+  internationalization: InternationalizationConfiguration;
+}
